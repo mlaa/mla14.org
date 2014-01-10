ff4615928e91ac1b12bcae3a56f730790131372e
Chris Zarate <chris@zarate.org>
2014-01-10 14:26:09 -0500

    Split application declaration and regions.

--- app/js/_init/application.js
+++ app/js/_init/application.js
@@ -2,9 +2,3 @@
 
 // Create the application.
 var MLA14 = new Backbone.Marionette.Application();
-
-// Add regions.
-MLA14.addRegions({
-  Content: '#content',
-  Updated: '#updated'
-});
---
+++ app/js/_init/regions.js
@@ -0,0 +1,7 @@
+/* Regions */
+
+// Add regions.
+MLA14.addRegions({
+  Content: '#content',
+  Updated: '#updated'
+});
